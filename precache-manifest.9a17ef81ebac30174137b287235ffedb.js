26e3a6820deb1fed490805964094d8bd98cc9e29
marzenakulikowska <kulikowska.marzenaa@gmail.com>
2020-07-24 15:57:21 +0200

    Updates

---
+++ precache-manifest.9a17ef81ebac30174137b287235ffedb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6615e4d7e7d501548e981b88cdd91f9",
+    "url": "/ADVISORY-project/index.html"
+  },
+  {
+    "revision": "ee76fff79881d2e449b4",
+    "url": "/ADVISORY-project/static/css/main.d604d066.chunk.css"
+  },
+  {
+    "revision": "f534e4d8d620f33ed8fa",
+    "url": "/ADVISORY-project/static/js/2.ab967b5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ADVISORY-project/static/js/2.ab967b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee76fff79881d2e449b4",
+    "url": "/ADVISORY-project/static/js/main.c5ff81d8.chunk.js"
+  },
+  {
+    "revision": "54429676cbce7d10f2e4",
+    "url": "/ADVISORY-project/static/js/runtime-main.756627c7.js"
+  },
+  {
+    "revision": "276d0579083459adaf7884c907b0f43f",
+    "url": "/ADVISORY-project/static/media/czyszczenie.276d0579.jpg"
+  },
+  {
+    "revision": "1fda12a855c3d6aef6c5ca33eb7824f3",
+    "url": "/ADVISORY-project/static/media/firma.1fda12a8.jpg"
+  },
+  {
+    "revision": "2ae4ae1b152e62a5e066d8a33eb0f267",
+    "url": "/ADVISORY-project/static/media/ksiegowosc.2ae4ae1b.jpg"
+  },
+  {
+    "revision": "9b36e334aefc8f4ce2bbff77c3623e83",
+    "url": "/ADVISORY-project/static/media/product.9b36e334.png"
+  },
+  {
+    "revision": "1c19818578c0453136dadce555974ca6",
+    "url": "/ADVISORY-project/static/media/pumpkin-seeds.1c198185.jpg"
+  },
+  {
+    "revision": "67255ac95b5f805ae937353708b22b8c",
+    "url": "/ADVISORY-project/static/media/pumpkin.67255ac9.jpg"
+  },
+  {
+    "revision": "bf0bdf98d350930468891ca7a4b06057",
+    "url": "/ADVISORY-project/static/media/sunset.bf0bdf98.jpg"
+  },
+  {
+    "revision": "6a3f4fced4ff6f716e26ef40694ff9e8",
+    "url": "/ADVISORY-project/static/media/transport.6a3f4fce.JPG"
+  }
+]);
